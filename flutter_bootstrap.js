c8a185f045b8c3226c3c32283e16e820fbaf93d0
EslamMohammad <eslamalsabbagh69@gmail.com>
2026-02-10 18:57:04 +0200

    new build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "983784957"
+    serviceWorkerVersion: "676528648"
   }
 });
