79ccdeb7c9b1cb50ff70c125a04a2512befa7078
counselmasters <eslamalsabbagh69@gmail.com>
2026-09-22 17:11:25 +0300

    Deploy web build - 2026-09-22_17:11

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "811307746" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2481876411" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
